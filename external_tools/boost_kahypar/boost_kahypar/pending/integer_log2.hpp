9b837e1cbff762cc1c2831120904a860b3eb7cab
GZZ <gzz_2000@126.com>
2025-06-13 22:00:39 +0800

    trim boost further

--- external_tools/boost_kahypar/boost_kahypar/pending/integer_log2.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BOOST_PENDING_INTEGER_LOG2_HPP
-#define BOOST_PENDING_INTEGER_LOG2_HPP
-
-#include <boost_kahypar/integer/integer_log2.hpp>
-#include <boost_kahypar/config/header_deprecated.hpp>
-
-BOOST_HEADER_DEPRECATED("<boost_kahypar/integer/integer_log2.hpp>");
-
-#endif
